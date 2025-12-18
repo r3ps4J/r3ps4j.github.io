e987ff8ab0fdb23d7135d3aa8fc469a78e8a8152
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-12-18 19:16:59 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import nextra from "nextra";
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true, // mandatory, otherwise won't export
+    },
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: "build"
+};
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+    // ... Add Nextra-specific options here
+});
+
+// Export the final Next.js config with Nextra included
+export default withNextra(nextConfig);
