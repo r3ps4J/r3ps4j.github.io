1c52c037088d7e4abe9f2d7d563374cb12c4bb95
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2025-12-19 22:38:06 +0100

    Setup tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import "../globals.css";
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
 import { Banner, Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
